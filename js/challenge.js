4fd0a2f72e4e5398f30fd8b4f0b28e93f8942034
mgranciano <granciano_2010@hotmail.com>
2023-04-15 18:38:01 -0600

    add function decrypt and validate fields

--- js/challenge.js
+++ js/challenge.js
@@ -1,28 +1,30 @@
-////
+const encryptText = ['ai', 'enter', 'imes', 'ober', 'ufat'];
+const decryptText = ['a', 'e', 'i', 'o', 'u'];
+const acceptedCharacters = '^[a-z ]+$';
+///
 //funcion para encriptar
 ////
 function encrypt(data) {
     let result = '';
     for (let index = 0; index < data.length; index++) {
-        const element = data[index];
-        switch (element) {
-            case 'a':
-                result = result + 'ai';
+        switch (data[index]) {
+            case decryptText[0]:
+                result = result + encryptText[0];
                 break;
-            case 'e':
-                result = result + 'enter';
+            case decryptText[1]:
+                result = result + encryptText[1];
                 break;
-            case 'i':
-                result = result + 'imse';
+            case decryptText[2]:
+                result = result + encryptText[2];
                 break;
-            case 'o':
-                result = result + 'ober';
+            case decryptText[3]:
+                result = result + encryptText[3];
                 break;
-            case 'u':
-                result = result + 'ufat';
+            case decryptText[4]:
+                result = result + encryptText[4];
                 break;
             default:
-                result = result + element;
+                result = result + data[index];
                 break;
         }
     }
@@ -34,9 +36,53 @@ function encrypt(data) {
 function decrypt(data) {
     let result = '';
 
-
-    result = data.replace('enter', 'e').replace('imes', 'i').replace('ai', 'a').replace('ober', 'o').replace('ufat', 'u');
-
+    for (let index = 0; index < data.length; index++) {
+        switch (data[index]) {
+            case decryptText[0]:
+                if ((data[index] + data[index + 1]) === encryptText[0]) {
+                    result = result + decryptText[0];
+                    index++;
+                } else {
+                    result = result + data[index];
+                }
+                break;
+            case decryptText[1]:
+                if ((data[index] + data[index + 1] + data[index + 2] + data[index + 3] + data[index + 4]) === encryptText[1]) {
+                    result = result + decryptText[1];
+                    index = index + 4;
+                } else {
+                    result = result + data[index];
+                }
+                break;
+            case decryptText[2]:
+                if ((data[index] + data[index + 1] + data[index + 2] + data[index + 3]) === encryptText[2]) {
+                    result = result + decryptText[2];
+                    index = index + 3;
+                } else {
+                    result = result + data[index];
+                }
+                break;
+            case decryptText[3]:
+                if ((data[index] + data[index + 1] + data[index + 2] + data[index + 3]) === encryptText[3]) {
+                    result = result + decryptText[3];
+                    index = index + 3;
+                } else {
+                    result = result + data[index];
+                }
+                break;
+            case decryptText[4]:
+                if ((data[index] + data[index + 1] + data[index + 2] + data[index + 3]) === encryptText[4]) {
+                    result = result + decryptText[4];
+                    index = index + 3;
+                } else {
+                    result = result + data[index];
+                }
+                break;
+            default:
+                result = result + data[index];
+                break;
+        }
+    }
     return result;
 }
 ////
@@ -50,18 +96,28 @@ function copyToClipboard() {
             console.log('Text copied to clipboard');
         })
         .catch(err => { // si manda error al llamar el api navigator manda el error en un alert 
-            Alert('Error in copying text: ', err);
+            alert('Error al copiar texto: ', err);
         });
 }
-
 ////
 // funcion para ocultar elementos al inicio de la pagina 
 ///
-function hide_show_Elements() {
+function hide_show_Elements(error) {
     document.getElementById('notresult').style.display = 'inline';
     document.getElementById('result').style.display = 'none';
     document.getElementById('result').value = '';
     document.getElementById("txtInput").focus();
+
+    if (error === 1) {
+        alert('El mensaje contiene caracteres inválidos');
+    }
+}
+////
+// funcion para ocultar elementos al inicio de la pagina 
+///
+function process_Elements() {
+    document.getElementById('result').style.display = 'inline';
+    document.getElementById('notresult').style.display = 'none';
 }
 ////
 //funcion para validar si Solo letras minúsculas y sin acentos, no acepta otra cosa 
@@ -70,8 +126,12 @@ function validate(value) {
     // Variable que usaremos para determinar si el input es valido
     let isValid = false;
     // regex para solo letras minusculas
-    const pattern = new RegExp('^[a-z]+$');
+    const pattern = new RegExp(acceptedCharacters);
 
+    if (!value) { // Se valida que tenga información el textarea 
+        alert('No hay datos para encriptar');
+        return;
+    }
 
     if (!pattern.test(value)) {
         isValid = false;
@@ -88,30 +148,30 @@ function validate(value) {
 function startEncript() {
     const data = document.getElementById("txtInput").value.trim();
 
-    if (!data) { // Se valida que tenga información el textarea 
-        alert('No hay datos para encriptar');
+    if (validate(data)) { // se valida que tenga los criterios correctos para encriptar
+
+        process_Elements();
+        document.getElementById("txtOutput").value = encrypt(data);
+
     } else {
-        if (validate(data)) { // se valida que tenga los criterios correctos para encriptar
-
-            document.getElementById('result').style.display = 'inline';
-            document.getElementById('notresult').style.display = 'none';
-            document.getElementById("txtOutput").value = encrypt(data);
-
-        } else {
-            alert('El mensaje a encriptar contiene caracteres inválidos');
-            hide_show_Elements();
-        }
+        hide_show_Elements(1);
     }
+
 }
 ////
 //funcion para iniciar la desencriptacion desencriptar
 ////
 function startDecrypt() {
-    const data = document.getElementById("txtInput").value;
+    const data = document.getElementById("txtInput").value.trim();
 
-    document.getElementById('result').style.display = 'inline';
-    document.getElementById('notresult').style.display = 'none';
-    document.getElementById("txtOutput").value = decrypt(data);
+    if (validate(data)) { // se valida que tenga los criterios correctos para encriptar
+
+        process_Elements();
+        document.getElementById("txtOutput").value = decrypt(data);
+
+    } else {
+        hide_show_Elements(1);
+    }
 }
 
 //Asignando la acción al boton de encriptar
@@ -126,6 +186,4 @@ btnDesencriptar.onclick = startDecrypt;
 var btnCopiar = document.getElementById("btnCopiar");
 btnCopiar.onclick = copyToClipboard;
 
-hide_show_Elements();
-
-// mandar msg con alert paraespecificar que solo lleva letrasy minusculas
+hide_show_Elements(0);
